9e820dcd58b68af7270a7ffb679779136362984a
blont714 <33482172+blont714@users.noreply.github.com>
2019-10-12 17:32:22 +0900

    Add files via upload

---
+++ Assets/Scripts/JankenController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JankenController : MonoBehaviour
+{
+    public GameObject Goo;
+    public GameObject Choki;
+    public GameObject Paa;
+    public GameObject MyGoo;
+    public GameObject MyChoki;
+    public GameObject MyPaa;
+
+    public void OnClickGoo()
+    {
+      this.MyGoo.SetActive(true);
+      this.MyChoki.SetActive(false);
+      this.MyPaa.SetActive(false);
+      this.EnemyHand();
+    }
+    
+    public void OnClickChoki()
+    {
+      this.MyGoo.SetActive(false);
+      this.MyChoki.SetActive(true);
+      this.MyPaa.SetActive(false);
+      this.EnemyHand();
+    }
+
+    public void OnClickPaa()
+    {
+      this.MyGoo.SetActive(false);
+      this.MyChoki.SetActive(false);
+      this.MyPaa.SetActive(true);
+      this.EnemyHand();
+    }
+
+
+
+    // Start is called before the first frame update
+    public void EnemyHand()
+    {
+        this.Goo.SetActive(false);
+        this.Choki.SetActive(false);
+        this.Paa.SetActive(false);
+
+        // 1:ぐー　2:ちょき　3:ぱー
+        int rand = Random.Range(1, 4);
+        if(rand == 1){
+            this.Goo.SetActive(true);
+        }else if(rand == 2){
+            this.Choki.SetActive(true);
+        }else if(rand == 3){
+            this.Paa.SetActive(true);
+        }
+    }
+  }
